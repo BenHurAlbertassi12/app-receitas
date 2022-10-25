f7e1c5a9ad41403fa4d0f1bf39b8425fdb64e23a
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-10-25 15:28:12 -0300

    Rotas criadas e teste da pagina de login iniciado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Switch } from 'react-router-dom';
-import LoginProvider from './Context/LoginProvider';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
+import LoginProvider from './Context/LoginProvider';
 import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import RecipeMeals from './pages/RecipeMeals';
+import RecipeDrinks from './pages/RecipeDrinks';
+import Profile from './pages/Profile';
+import MealsInProgress from './pages/MealInProgress';
+import DrinksInProgress from './pages/DrinkInProgress';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoritesRecipes';
 
 function App() {
   return (
     <LoginProvider>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" component={ Meals } />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route path="/drinks" component={ Drinks } />
+          <Route path="/meals/:id-da-receita" component={ RecipeMeals } />
+          <Route path="/drinks/:id-da-receita" component={ RecipeDrinks } />
+          <Route path="/meals/:id-da-receita/in-progress" component={ MealsInProgress } />
+          <Route
+            path="/drinks:id-da-receita/in-progress"
+            component={ DrinksInProgress }
+          />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/done-recipes" component={ DoneRecipes } />
+          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        </Switch>
+      </BrowserRouter>
     </LoginProvider>
   );
 }
