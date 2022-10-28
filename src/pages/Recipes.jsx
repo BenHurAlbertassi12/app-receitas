602a9243108e5c4cda4175fe09538c875a420805
Pedro Nunes <pedro.icn88@gmail.com>
2022-10-28 18:21:16 -0300

    Main group 24 Requisito 19

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../component/Footer';
+import InitialDrinksRecipes from '../component/InitialDrinksRecipes';
+import InitialMealsRecipes from '../component/InitialMealsRecipes';
 
 export default function Recipes() {
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  const renderInitialRecipes = () => {
+    switch (pathname) {
+    case '/meals':
+      return <InitialMealsRecipes />;
+    case '/drinks':
+      return <InitialDrinksRecipes />;
+    default:
+      return null;
+    }
+  };
   return (
     <div>
       <span>Recipes</span>
+      {
+        renderInitialRecipes()
+      }
       <Footer />
     </div>
   );
