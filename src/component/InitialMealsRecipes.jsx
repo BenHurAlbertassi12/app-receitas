9593f7c584c5927412e1f7cd4344e1a478cb0923
Pedro Nunes <pedro.icn88@gmail.com>
2022-10-28 20:13:52 -0300

    Main group 24 Requisito 19 sem os testes

--- src/component/InitialMealsRecipes.jsx
+++ src/component/InitialMealsRecipes.jsx
@@ -1,11 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import LoginContext from '../Context/LoginContext';
 
 function InitialMealsRecipes() {
   const { initialMeals } = useContext(LoginContext);
+  const [categoryMeal, setCategoryMeal] = useState([]);
   const numberMaxArray = 11;
+  const numberMaxCategory = 4;
+
+  const arrayCategoryMeal = categoryMeal
+    .filter((_item, index) => index <= numberMaxCategory);
+
   const arrayInitialMeals = initialMeals
     .filter((_item, index) => index <= numberMaxArray);
+
   const listMeals = arrayInitialMeals
     .map((meal) => {
       const formatMeals = Object.entries(meal);
@@ -17,8 +24,30 @@ function InitialMealsRecipes() {
         }, {});
       return filteredMeals;
     });
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const data = await response.json();
+      setCategoryMeal(data.meals);
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <div>
+      <h2>Categorias</h2>
+      {
+        arrayCategoryMeal.map((category) => (
+          <button
+            type="button"
+            key={ category.strCategory }
+            data-testid={ `${category.strCategory}-category-filter` }
+          >
+            { category.strCategory }
+          </button>
+        ))
+      }
       {
         listMeals.map((meal, index) => (
           <section data-testid={ `${index}-recipe-card` } key={ meal.idMeal }>
