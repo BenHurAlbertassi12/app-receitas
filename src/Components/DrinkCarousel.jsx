5e2ffba2c309dea6f9a45f65dc580c03ee8f3dc1
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-10-28 10:50:26 -0300

    Requisito 27 realizado. Precisa arrumar CSS

--- src/Components/DrinkCarousel.jsx
+++ src/Components/DrinkCarousel.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function MealCarousel() {
   const [dataDrinkApi, setDataDrinkApi] = useState([]);
 
   const requestDrinkApi = async () => {
-    const randomNumber = 0.5;
+    // const randomNumber = 0.5;
     const maxNumber = 6;
     const drinkApi = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
     const responseDrinkApi = await drinkApi.json();
     const drinkRecipeResponse = responseDrinkApi?.drinks;
-    const randomDrinks = drinkRecipeResponse?.sort(() => randomNumber - Math.random());
-    const maxDrinks = randomDrinks?.slice(0, maxNumber);
+    // const randomDrinks = drinkRecipeResponse?.sort(() => randomNumber - Math.random());
+    const maxDrinks = drinkRecipeResponse?.slice(0, maxNumber);
     setDataDrinkApi(await maxDrinks);
   };
 
@@ -18,6 +20,15 @@ export default function MealCarousel() {
     requestDrinkApi();
   }, []);
   return (
-    <div />
+    <div style={ { width: 300, heigth: 300 } }>
+      <Carousel thumbWidth={ 300 } dynamicHeight={ 200 }>
+        {dataDrinkApi?.map((recipe, index) => (
+          <Carousel.Item data-testid={ `${index}-recommendation-card` } key={ index }>
+            <h6 data-testid={ `${index}-recommendation-title` }>{recipe.strDrink}</h6>
+            <img style={ {} } src={ recipe.strDrinkThumb } alt={ recipe.strDrink } />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
--- src/Components/MealCarousel.jsx
+++ src/Components/MealCarousel.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function DrinkCarousel() {
-  const [dataMealApi, setDataMealApi] = useState({});
+  const [dataMealApi, setDataMealApi] = useState([]);
   const requestMealApi = async () => {
-    const randomNumber = 0.5;
-    const maxNumber = 6;
+    // const randomNumber = 0.5;
+    const maxDrinks = 6;
     const mealApi = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     const responseMealApi = await mealApi.json();
     const mealRecipeResponse = responseMealApi.meals;
-    const randomMeals = mealRecipeResponse?.sort(() => randomNumber - Math.random());
-    const maxMeals = randomMeals?.slice(0, maxNumber);
+    // const randomMeals = mealRecipeResponse?.sort(() => randomNumber - Math.random());
+    const maxMeals = mealRecipeResponse?.slice(0, maxDrinks);
+    console.log(maxMeals);
     setDataMealApi(maxMeals);
   };
 
@@ -17,6 +20,15 @@ export default function DrinkCarousel() {
     requestMealApi();
   }, []);
   return (
-    <div>DrinkCarousel</div>
+    <div style={ { width: 300, heigth: 300 } }>
+      <Carousel thumbWidth={ 300 } dynamicHeight={ 200 }>
+        {dataMealApi?.map((recipe, index) => (
+          <Carousel.Item data-testid={ `${index}-recommendation-card` } key={ index }>
+            <h6 data-testid={ `${index}-recommendation-title` }>{recipe.strMeal}</h6>
+            <img style={ {} } src={ recipe.strMealThumb } alt={ recipe.strMeal } />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }
