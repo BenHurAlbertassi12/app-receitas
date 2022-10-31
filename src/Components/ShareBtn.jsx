f67cb34f9b6604c6aee545b54fde27cedfb57d22
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-10-31 15:37:27 -0300

    Requisito 33 realizado

--- src/Components/ShareBtn.jsx
+++ src/Components/ShareBtn.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const copy = require('clipboard-copy');
 
 export default function ShareBtn() {
+  const [linkCopied, setLinkCopied] = useState(false);
+  const handleClick = () => {
+    copy(window.location.href);
+    setLinkCopied(true);
+  };
   return (
     <div>
-      <button type="button" data-testid="share-btn">Share</button>
+      <button type="button" data-testid="share-btn" onClick={ handleClick }>Share</button>
+      {linkCopied && <span>Link copied!</span>}
     </div>
   );
 }
