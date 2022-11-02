b653d26424a3666c37850c9c36979e738bdef6d5
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-11-02 18:25:35 -0300

    Requisito 49 realizado, faltando testes

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import HeaderDois from '../component/HeaderDois';
 import shareIcon from '../images/shareIcon.svg';
 
 const copy = require('clipboard-copy');
 
 function DoneRecipes() {
-  // const getDoneRecipes = [
+  // const getDoneRecipesMock = [
   //   {
   //     id: '52771',
   //     type: 'meal',
@@ -29,29 +30,77 @@ function DoneRecipes() {
   //     tags: [],
   //   },
   // ];
-
-  const getDoneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  // localStorage.setItem('doneRecipes', JSON.stringify('getDoneRecipesMock'));
+  const [getDoneRecipes, setGetDoneRecipes] = useState([]);
   const [linkCopied, setLinkCopied] = useState(false);
 
-  const handleClick = (type, id) => {
+  useEffect(() => {
+    const getDoneRecipesLocal = JSON.parse(localStorage.getItem('doneRecipes'));
+    setGetDoneRecipes(getDoneRecipesLocal);
+  }, []);
+
+  const handleClipBoard = (type, id) => {
     copy(`${window.location.origin}/${type}s/${id}`);
     setLinkCopied(true);
+  };
+
+  const handleFilter = ({ target }) => {
+    const getDoneRecipesLocal = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (target.name === 'Meals') {
+      const mealFilter = getDoneRecipesLocal
+        .filter((doneRecipe) => doneRecipe.type === 'meal');
+      setGetDoneRecipes(mealFilter);
+    } else if (target.name === 'Drinks') {
+      const drinkFilter = getDoneRecipesLocal
+        .filter((doneRecipe) => doneRecipe.type === 'drink');
+      setGetDoneRecipes(drinkFilter);
+    } else {
+      setGetDoneRecipes(getDoneRecipesLocal);
+    }
   };
 
   return (
     <div>
       <HeaderDois />
       <title data-testid="page-title" title="Done Recipes">Done Recipes</title>
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-meal-btn">Meals</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <button
+        name="All"
+        onClick={ handleFilter }
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+
+      </button>
+      <button
+        name="Meals"
+        type="button"
+        onClick={ handleFilter }
+        data-testid="filter-by-meal-btn"
+      >
+        Meals
+
+      </button>
+      <button
+        name="Drinks"
+        type="button"
+        onClick={ handleFilter }
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+
+      </button>
       {getDoneRecipes?.map((doneRecipe, index) => (
         <section key={ index }>
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            src={ doneRecipe.image }
-            alt={ doneRecipe.name }
-          />
+          <Link to={ `/${doneRecipe.type}s/${doneRecipe.id}` }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ doneRecipe.image }
+              alt={ doneRecipe.name }
+              width="300px"
+            />
+            <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</p>
+          </Link>
           {doneRecipe.type === 'meal'
             ? (
               <p data-testid={ `${index}-horizontal-top-text` }>
@@ -64,7 +113,6 @@ function DoneRecipes() {
               </p>
             )}
 
-          <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</p>
           <p data-testid={ `${index}-horizontal-done-date` }>{doneRecipe.doneDate}</p>
           {doneRecipe.tags?.map((tag, i) => (
             <p
@@ -78,7 +126,7 @@ function DoneRecipes() {
             type="button"
             data-testid={ `${index}-horizontal-share-btn` }
             src={ shareIcon }
-            onClick={ () => handleClick(doneRecipe.type, doneRecipe.id) }
+            onClick={ () => handleClipBoard(doneRecipe.type, doneRecipe.id) }
           >
             <img src={ shareIcon } alt="share-button" />
           </button>
