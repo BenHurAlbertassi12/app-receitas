56c213c06516c0cc34e2518e00c82252729896a9
Bernardo Lavinas <106279580+BLavinas@users.noreply.github.com>
2022-11-03 12:54:35 -0300

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from '../Components/SearchBar';
+import SearchBar from '../components/SearchBar';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
